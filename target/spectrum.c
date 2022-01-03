39c0e8f8667996debdd12076c13a8acc2d423d5e
orterforth <orterforth@users.noreply.github.com>
2022-01-03 17:23:44 +0000

    ZX Spectrum support in C

---
+++ target/spectrum.c
@@ -0,0 +1,120 @@
+/* SYSTEM BINDINGS */
+
+#include <rs232.h>
+#include <spectrum.h>
+
+#include "rf.h"
+
+void rf_init()
+{
+  /* disc controller over RS232 */
+  if (rs232_params(RS_BAUD_9600 | RS_BITS_8 | RS_STOP_1, RS_PAR_NONE) != RS_ERR_OK) {
+    exit(1);
+  }
+  if (rs232_init() != RS_ERR_OK) {
+    exit(1);
+  }
+}
+
+void rf_code_emit()
+{
+  RF_START;
+  {
+    unsigned char c;
+    
+    c = RF_SP_POP & 0x7F;
+    fputc_cons(c);
+    RF_USER_OUT++;
+  }
+  RF_JUMP_NEXT;
+}
+
+void __FASTCALL__ rf_out(char c)
+{
+  fputc_cons(c);
+}
+
+static char capslock = 0;
+
+void rf_code_key(void)
+{
+  RF_START;
+  {
+    int c;
+
+    /* show cursor */
+    fputc_cons('_');
+    fputc_cons(32);
+    fputc_cons(8);
+    fputc_cons(8);
+
+    /* caps lock */
+    while ((c = fgetc_cons()) == 6) {
+      capslock ^= 1;
+    }
+
+    /* eof */
+    if (c == -1) {
+      exit(0);
+    }
+
+    /* LF to CR */
+    if (c == 10) {
+      c = 13;
+    }
+
+    /* apply caps lock */
+    if (capslock && c > 96 && c < 123) {
+      c ^= 0x20;
+    }
+
+    /* return key */
+    RF_SP_PUSH(c & 0x7F);
+  }
+  RF_JUMP_NEXT;
+}
+
+void rf_code_qterm(void)
+{
+  RF_START;
+  RF_SP_PUSH(in_KeyPressed(0x817F));
+  RF_JUMP_NEXT;
+}
+
+void rf_code_cr()
+{
+  RF_START;
+  fputc_cons(10);
+  RF_JUMP_NEXT;
+}
+
+void rf_disc_read(char *c, unsigned char len)
+{
+  ++len;
+  while (--len) {
+    while (rs232_get(c) == RS_ERR_NO_DATA) {
+    }
+    c++;
+  }
+}
+
+void rf_disc_write(char *p, unsigned char len)
+{
+  ++len;
+  while (--len) {
+    while (rs232_put(*p) == RS_ERR_OVERFLOW) {
+    }
+    p++;
+  }
+}
+
+void rf_disc_flush(void)
+{
+}
+
+void rf_fin(void)
+{
+  if (rs232_close() != RS_ERR_OK) {
+    exit(1);
+  }
+}
