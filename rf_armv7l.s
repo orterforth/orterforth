cd6089c25a0b15bfc4bedbb6e35d55a8bcf22788
orterforth <orterforth@users.noreply.github.com>
2022-06-05 21:22:27 +0100

    armv7l lit, exec, bran, zbran

--- rf_armv7l.s
+++ rf_armv7l.s
@@ -11,74 +11,108 @@
 	.eabi_attribute 18, 4
 	.text
 
+@	.data
+@	.align	2
+@savefp:
+@	.space	4
+@
+@	.align	2
+@savesp:
+@	.space	4
+
+	.text
 	.align	2
 	.global	_rf_trampoline
-	.arch armv6
-	.syntax unified
-	.arm
-	.fpu vfp
-	.type	_rf_trampoline, %function
 _rf_trampoline:
 
 	push	{fp, lr}
 
 .trampoline1:
 
-	ldr	r3, .trampoline3
+	ldr	r3, =rf_fp
 	ldr	r3, [r3]
 	cmp	r3, #0
 	beq	.trampoline2
 
-	ldr	r10, .trampoline4       @ IP into r10
+	ldr	r10, =rf_ip             @ IP into r10
 	ldr	r10, [r10]
-	ldr	r9, .trampoline5        @ W into r9
+	ldr	r9, =rf_w               @ W into r9
 	ldr	r9, [r9]
+
+	@ ldr r0, =savesp             @ save sp and fp
+	@ str r13, [r0]
+	@ ldr r0, =savefp
+	@ str r11, [r0]
+	ldr r0, =rf_sp
+	ldr r8, [r0]                @ SP into r8 (for now)
 
 	blx	r3
 	b .trampoline1
 
 .trampoline2:
-	nop
+
 	pop	{fp, pc}
 
 	.align	2
-.trampoline3:
-	.word	rf_fp
-
-	.align	2
-.trampoline4:
-	.word	rf_ip
-
-	.align	2
-.trampoline5:
-	.word	rf_w
-
-	.size	_rf_trampoline, .-_rf_trampoline
-
-	.align	2
 	.global	_rf_start
-	.syntax unified
-	.arm
-	.fpu vfp
-	.type	_rf_start, %function
 _rf_start:
 
-	ldr r3, .trampoline4
+	ldr r3, =rf_ip
 	str	r10, [r3]               @ r10 into IP
-	ldr r3, .trampoline5
+	ldr r3, =rf_w
 	str	r9, [r3]                @ r9 into W
+	ldr r3, =rf_sp
+	str	r8, [r3]                @ r8 into SP
 
 	bx	lr
 	.size	_rf_start, .-_rf_start
 
+dpush:
+
+	str r1, [r8,#-4]!
+
+apush:
+
+	str r0, [r8,#-4]!
+
 	.align	2
 	.global	rf_next
-	.syntax unified
-	.arm
-	.fpu vfp
-	.type	rf_next, %function
 rf_next:
-
+next:
 	ldr r9, [r10], #4
-	ldr	r3, [r9]
-	bx r3
+next1:
+	ldr	r0, [r9]
+	bx r0
+
+	.align	2
+	.global	rf_code_lit
+rf_code_lit:
+
+	ldr r0, [r10], #4           @ AX <- LITERAL
+	b apush                     @ TO TOP OF STACK
+
+	.align	2
+	.global	rf_code_exec
+rf_code_exec:
+
+	ldr r9, [r8], #4            @ GET CFA
+	b next1                     @ EXECUTE NEXT
+
+	.align	2
+	.global	rf_code_bran
+rf_code_bran:
+
+bran1:
+	ldr r0, [r10]
+	add r10, r10, r0            @ (IP) <- (IP) + ((IP))
+	b next                      @ JUMP TO OFFSET
+
+	.align	2
+	.global	rf_code_zbran
+rf_code_zbran:
+
+	ldr r0, [r8], #4            @ GET STACK VALUE
+	orrs r0, r0                 @ ZERO?
+	beq bran1                   @ YES, BRANCH
+	add r10, r10, #4            @ NO, CONTINUE...
+	b next
