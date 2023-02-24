3b7081a50fecf29b25aee04a4f6300a38aa1ca2c
orterforth <orterforth@users.noreply.github.com>
2023-02-24 20:47:18 +0000

    start on Model 100 hw

--- target/m100/m100.mk
+++ target/m100/m100.mk
@@ -4,6 +4,21 @@ m100 :
 
 	mkdir $@
 
+.PHONY : m100-hw
+m100-hw : m100/hw.ba
+
+	@printf '  \033[1;35mOn the target type RUN "COM:38N1D" <enter>\033[0;0m\n'
+	@printf '  \033[1;35mThen on here press <enter>\033[0;0m\n'
+	@read LINE
+	@$(ORTER) serial -e 5 $(SERIALPORT) 300 < $<
+
+m100/hw.ba : | m100
+
+	printf '\r\n\r\n10 PRINT "Hello World ";\r\n' > $@.io
+	printf '20 GOTO 10\r\n' >> $@.io
+	printf '\r\n\032' >> $@.io
+	mv $@.io $@
+
 m100/hw.co : | m100
 
 	zcc +m100 -subtype=default hw.c -o $@ -create-app
