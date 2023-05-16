c68d0698374f9649ce09a4b520f50c6ebee391e6
orterforth <orterforth@users.noreply.github.com>
2023-05-16 22:41:20 +0100

    move Mandelbrot to library

--- library.f
+++ library.f
@@ -143,7 +143,7 @@ CR fac list CR
 
 ;S
 ( Reverse a string                                            )
-12 LOAD                        ( load str vocabulary          )
+16 LOAD                        ( load str vocabulary          )
 FORTH DEFINITIONS
 : s1 str " Hello World, this is a string." ;
 s1 C@ str new CONSTANT s2
@@ -190,6 +190,70 @@ FORTH DEFINITIONS VOCABULARY roman IMMEDIATE roman DEFINITIONS
 
 
 ;S
+( Mandelbrot - derived from fract.fs in openbios              )
+HEX
+
+( compare double numbers                                      )
+: D=                            ( d1 d2 -- f )
+  ROT = >R = R> AND ;           ( compare high and low words  )
+
+( system specific screen width                                )
+: columns                       ( -- u )
+  tg     3948. D= IF 28 ;S ENDIF ( BBC 40 columns             )
+  tg 3195C1F7. D= IF 20 ;S ENDIF ( Dragon 32 columns          )
+  tg 6774E16F. D= IF 20 ;S ENDIF ( Spectrum 32 columns        )
+  50                            ( default 80 columns          )
+;
+
+-->
+( Mandelbrot - derived from fract.fs in openbios              )
+
+( system specific screen height                               )
+: rows                           ( -- u )
+  tg     3948. D= IF 19 ;S ENDIF ( BBC 25 rows                )
+  tg 3195C1F7. D= IF 10 ;S ENDIF ( Dragon 16 rows             )
+  tg 6774E16F. D= IF 18 ;S ENDIF ( Spectrum 24 rows           )
+  18                            ( default 24 rows             )
+;
+
+
+
+
+
+
+-->
+( Mandelbrot - derived from fract.fs in openbios              )
+: mandelbrot
+    CR
+    466 DUP MINUS DO            ( y axis                      )
+        I
+        400 DUP DUP + MINUS DO  ( x axis                      )
+            2A 0 DUP OVER OVER  ( * 0 0 0 0                   )
+            1E 0 DO             ( iterate                     )
+                ROT >R ROT R> 200 */ ( 2SWAP * 200 / )
+                5 cs SP@ + @ +  ( 4 PICK +                    )
+                ROT ROT -       ( -ROT - )
+                R> R> R SWAP >R SWAP >R ( J                   )
+                +
+                DUP DUP 400 */ ROT
+                DUP DUP 400 */ ROT
+-->
+( Mandelbrot - derived from fract.fs in openbios              )
+                SWAP OVER OVER + 1000 > IF ( if diverging     )
+                    DROP DROP DROP DROP DROP
+                    BL 0 DUP OVER OVER LEAVE ( space          )
+                ENDIF
+            LOOP
+            DROP DROP DROP DROP
+            EMIT                ( * or space                  )
+        [ 400 3 * columns 2 - / ] ( compute step from cols    )
+        LITERAL +LOOP
+        CR DROP                 ( end of line                 )
+    [ 466 2+ 2 * rows / ] LITERAL +LOOP ;
+
+mandelbrot DECIMAL ;S
+
+
 ( str: Forth/Pascal string handling                           )
 FORTH DEFINITIONS VOCABULARY str IMMEDIATE str DEFINITIONS
 : (") R> DUP COUNT + >R ;       ( return string and advance IP)
