334c27e00e65009405bf5dd145c042a94374aa39
orterforth <orterforth@users.noreply.github.com>
2023-06-07 21:12:49 +0100

    mux towards Pico

--- mux.c
+++ mux.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
+#ifdef __RC2014
+#include <z80.h>
+#endif
 
 #include "rf.h"
+
+#ifdef PICO
+#define RF_SLEEP(a) sleep_ms(a);
+#endif
+#ifdef __RC2014
+#define RF_SLEEP(a) z80_delay_ms(a);
+#endif
 
 void rf_code_emit(void)
 {
   RF_START;
   {
-    unsigned char c;
+    int c = RF_SP_POP & 0x7F;
     
-    c = RF_SP_POP & 0x7F;
-    putchar(c);
+    /* write char, wait if serial disconnected */
+    while (putchar(c) == -1) {
+      RF_SLEEP(1000);
+    }
+
+    /* backspace erase */
     if (c == 0x08) {
       putchar(' ');
       putchar(c);
     }
+
     RF_USER_OUT++;
   }
   RF_JUMP_NEXT;
@@ -25,9 +40,14 @@ void rf_code_key(void)
   {
     int c;
 
+    /* read char, wait if serial disconnected */
     /* skip disc input */
-    while (((c = getchar()) & 0x80)) {
-    }
+    /* TODO getchar to work with Pico */
+    do {
+      if ((c = fgetc(stdin)) == -1) {
+        RF_SLEEP(1000);
+      }
+    } while (c == -1 || c & 0x80);
 
     /* LF to CR */
     if (c == 10) {
@@ -49,24 +69,26 @@ void rf_code_qterm(void)
 void rf_code_cr(void)
 {
   RF_START;
-  putchar(10);
+  while (putchar(10) == -1) {
+    RF_SLEEP(1000);
+  }
   RF_JUMP_NEXT;
 }
 
-void rf_disc_read(char *c, unsigned char len)
+void rf_mux_disc_read(char *c, unsigned char len)
 {
   for (; len; len--) {
     /* skip keyboard input */
-    while (!((*c = getchar()) & 0x80)) {
+    /* TODO getchar to work with Pico */
+    while (!((*c = fgetc(stdin)) & 0x80)) {
     }
     *(c++) &= 0x7F;
   }
 }
 
-void rf_disc_write(char *p, unsigned char len)
+void rf_mux_disc_write(char *c, unsigned char len)
 {
   for (; len; len--) {
-    putchar(*(p++) | 0x80);
+    putchar(*(c++) | 0x80);
   }
 }
-
--- target/rc2014/system.c
+++ target/rc2014/system.c
@@ -1,7 +1,21 @@
 /* SYSTEM BINDINGS */
 
+void rf_mux_disc_read(char *c, unsigned char len);
+
+void rf_mux_disc_write(char *c, unsigned char len);
+
 void rf_init(void)
 {
+}
+
+void rf_disc_read(char *c, unsigned char len)
+{
+  rf_mux_disc_read(c, len);
+}
+
+void rf_disc_write(char *c, unsigned char len)
+{
+  rf_mux_disc_write(c, len);
 }
 
 void rf_fin(void)
