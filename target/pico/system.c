50f8e47c9446c77f7d00a0511a9d2023440fe6cc
orterforth <orterforth@users.noreply.github.com>
2023-06-25 17:58:49 +0100

    remove commented code

--- target/pico/system.c
+++ target/pico/system.c
@@ -8,9 +8,6 @@
 /* use heap memory */
 char *rf_origin = 0;
 
-/* auto command for boot purposes */
-char *rf_system_auto_cmd = 0;
-
 void rf_init(void)
 {
   /* init UART */
@@ -18,6 +15,7 @@ void rf_init(void)
 
   /* allocate memory */
   rf_origin = malloc(RF_MEMORY_SIZE);
+  /* on failure, repeat message */
   if (!rf_origin) {
     for (;;) {
       perror("memory init failed");
@@ -25,77 +23,6 @@ void rf_init(void)
     }
   }
 }
-
-#ifdef xxxx
-void rf_code_emit(void)
-{
-  RF_START;
-  {
-    int c = RF_SP_POP & 0x7F;
-
-    /* write char, wait if serial disconnected */
-    while (putchar(c) == -1) {
-      sleep_ms(1000);
-    }
-
-    /* backspace erase */
-    if (c == 0x08) {
-      putchar(' ');
-      putchar(c);
-    }
-
-    RF_USER_OUT++;
-  }
-  RF_JUMP_NEXT;
-}
-#endif
-
-#ifdef xxxx
-void rf_code_key(void)
-{
-  RF_START;
-  {
-    int c;
-
-    if (rf_system_auto_cmd && *rf_system_auto_cmd) {
-      c = *(rf_system_auto_cmd++);
-    } else {
-
-      /* read char, wait if serial disconnected */
-      while ((c = fgetc(stdin)) == -1) {
-        sleep_ms(1000);
-      }
-
-      /* LF to CR */
-      if (c == 10) {
-        c = 13;
-      }
-    }
-
-    /* return key */
-    RF_SP_PUSH(c & 0x7F);
-  }
-  RF_JUMP_NEXT;
-}
-#endif
-
-#ifdef xxxx
-void rf_code_qterm(void)
-{
-  RF_START;
-  RF_SP_PUSH(0);
-  RF_JUMP_NEXT;
-}
-#endif
-
-#ifdef xxxx
-void rf_code_cr(void)
-{
-  RF_START;
-  putchar('\n');
-  RF_JUMP_NEXT;
-}
-#endif
 
 char rf_system_local_disc = 1;
 
@@ -106,11 +33,6 @@ void rf_disc_read(char *p, uint8_t len)
       *(p++) = rf_persci_getc();
     }
   } else {
-/*
-    for (; len; --len) {
-      *(p++) = fgetc(stdin) & 0x7F;
-    }
-*/
     rf_mux_disc_read(p, len);
   }
 }
@@ -120,6 +42,7 @@ void rf_disc_write(char *p, uint8_t len)
   if (rf_system_local_disc) {
     for (; len; --len) {
       if (rf_persci_putc(*(p++)) == -1) {
+        /* on failure, repeat message */
         for (;;) {
           fprintf(stderr, "rf_persci_putc invalid state\n");
           sleep_ms(1000);
@@ -127,11 +50,6 @@ void rf_disc_write(char *p, uint8_t len)
       }
     }
   } else {
-/*
-    for (; len; --len) {
-      putchar(*(p++) | 0x80);
-    }
-*/
     rf_mux_disc_write(p, len);
   }
 }
