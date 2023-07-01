4827ecbb41b0ee8e3d4cbebee5ead8ed2c38e68a
orterforth <orterforth@users.noreply.github.com>
2023-07-01 21:21:10 +0100

    first cut at C64

--- target/c64/c64.mk
+++ target/c64/c64.mk
@@ -12,38 +12,72 @@ c64-clean :
 
 	rm -rf c64/*
 
-.PHONY : c64-example
-c64-example : ../c64-up2400-cc65/example/example.d64
-
-	x64 -userportdevice 2 -rsuserdev 3 -rsuserbaud 2400 -rsdev4 "|echo 'hello'" -rsdev4baud 2400 -autostart "$<:example.prg"
-
 .PHONY : c64-hw
 c64-hw : c64/hw.prg
 
-	x64 -userportdevice 2 -rsuserdev 3 -rsuserbaud 2400 -rsdev4 "|$(DISC) model.img data.img" -rsdev4baud 2400 -autostartprgmode 1 -autostart $<
+	x64 \
+		-kernal roms/c64p/901227-02.u4 \
+		-basic roms/c64p/901226-01.u3 \
+		-chargen roms/c64p/901225-01.u5 \
+		+saveres +confirmonexit -autostartprgmode 1 -autostart $<
 
+# TODO cfg file with sections for inst
 .PHONY : c64-run
-c64-run : c64/inst.prg
+c64-run : c64/orterforth.prg
 
-	# start disc
-	$(START) disc.pid $(DISC) tcp 25232 model.img data.img
+	# x64 \
+	# 	-kernal roms/c64p/901227-02.u4 \
+	# 	-basic roms/c64p/901226-01.u3 \
+	# 	-chargen roms/c64p/901225-01.u5 \
+	# 	+warp +saveres +confirmonexit \
+	# 	-userportdevice 2 -rsuserdev 2 -rsuserbaud 2400 -rsdev3baud 2400 \
+	# 	-autostartprgmode 1 -autostart $<
 
-	# x64 -userportdevice 2 -rsuserdev 3 -rsuserbaud 2400 -rsdev4 "|$(DISC) model.img data.img" -rsdev4baud 2400 -autostartprgmode 1 -autostart $<
-	x64 -userportdevice 2 -rsuserdev 2 -rsuserbaud 2400 -rsdev3baud 2400 -autostartprgmode 1 -autostart $<
+c64/orterforth.hex : c64/inst.prg model.img | $(DISC)
 
-	@$(STOPDISC)
+	@$(CHECKMEMORY) 0x801 0x3300 $(shell $(STAT) c64/inst.prg)
 
+	rm -f $@.io
+	touch $@.io
+
+	$(START) disc.pid $(DISC) tcp 25232 model.img $@.io
+
+	$(START) vice.pid x64 \
+		-kernal roms/c64p/901227-02.u4 \
+		-basic roms/c64p/901226-01.u3 \
+		-chargen roms/c64p/901225-01.u5 \
+		-warp +saveres +confirmonexit \
+		-userportdevice 2 -rsuserdev 2 -rsuserbaud 2400 -rsdev3baud 2400 \
+		-autostartprgmode 1 -autostart $<
+
+	@$(WAITUNTILSAVED) $@.io
+
+#	@printf '* \033[1;33mStopping Vice\033[0;0m\n'
+#	@sh scripts/stop.sh vice.pid
+
+#	$(STOPDISC)
+
+#	mv $@.io $@
+
+# TODO specific rules listing all dependencies
 # general assemble rule
 c64/%.o : c64/%.s
 
 	ca65 -t c64 -o $@ $<
 
+# TODO rename to c64.inc
+# TODO pass ORIGIN
+# TODO hard code instead
+#
 # general compile rule
 c64/%.s : %.c | c64
 
 	cc65 -O -t c64 -DRF_TARGET_INC='"target/c64/default.inc"' -o $@ $<
 
-c64/c64-up2400.s : c64/c64-up2400.ser
+# serial driver
+# TODO diagnose issue with driver built from source
+#c64/c64-up2400.s : tools/github.com/nanoflite/c64-up2400-cc65/driver/c64-up2400.ser
+c64/c64-up2400.s : target/c64/c64-up2400.ser
 
 	co65 --code-label _c64_serial -o $@ $<
   
@@ -61,3 +95,23 @@ c64/hw.prg : hw.c | c64
 c64/inst.prg : c64/main.o c64/rf.o c64/inst.o c64/rf_system_c.o c64/c64-up2400.o | c64
 
 	cl65 -O -t c64 -o $@ -m c64/inst.map $^
+
+c64/orterforth : c64/orterforth.hex | $(ORTER)
+
+	$(ORTER) hex read < $< > $@.io
+	mv $@.io $@
+
+c64/orterforth.prg : c64/orterforth
+
+	printf '\001\010' > $@.io
+	cat $< >> $@.io
+	mv $@.io $@
+
+# Johan's serial driver
+tools/github.com/nanoflite/c64-up2400-cc65/driver/c64-up2400.s :
+
+	git submodule update --init tools/github.com/nanoflite/c64-up2400-cc65
+
+tools/github.com/nanoflite/c64-up2400-cc65/driver/c64-up2400.ser : tools/github.com/nanoflite/c64-up2400-cc65/driver/c64-up2400.s
+
+	cd tools/github.com/nanoflite/c64-up2400-cc65 && make
---
+++ target/c64/system.c
@@ -0,0 +1,130 @@
+/* SYSTEM BINDINGS */
+
+/*
+#include <cbm.h>
+*/
+#include <conio.h>
+#include <serial.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "../../rf.h"
+
+static const struct ser_params serial_params = {
+    SER_BAUD_2400,
+    SER_BITS_8,
+    SER_STOP_1,
+    SER_PAR_NONE,
+    SER_HS_NONE
+};
+
+extern char c64_serial;
+  
+void rf_init(void)
+{
+  /* conio enable cursor */
+  cursor(1);
+
+  /* init serial */
+  if (ser_install(&c64_serial)) {
+  }
+  if (ser_open(&serial_params)) {
+  }
+  if (ser_ioctl(1, NULL)) {
+  }
+  /* TODO deal with bad return values */
+}
+
+void rf_code_emit(void)
+{
+  RF_START;
+  {
+    uint8_t c;
+    
+    c = RF_SP_POP & 0x7F;
+    /* PETSCII swap case */
+    if (((c & 0xDF) >= 'A' && (c & 0xDF) <= 'Z')) {
+      c ^= 0x20;
+    }
+    /* BS */
+    if (c == 0x08) {
+      c = 0x14;
+    }
+    putchar(c);
+    RF_USER_OUT++;
+  }
+  RF_JUMP_NEXT;
+}
+
+void rf_code_key(void)
+{
+  RF_START;
+  {
+    int c;
+
+    /* get key */
+/*
+    c = getchar();
+*/
+    c = cgetc();
+/*
+    while ((c = cbm_k_getin()) == 0) { }
+*/
+    /* BS */
+/*
+    if (c == 0x08) {
+      cputc(' ');
+      cputc(c);
+    }
+*/
+/*
+    if (c < 32) printf("[%02X] ", c);
+*/
+    /* return key */
+    RF_SP_PUSH(c & 0x7F);
+  }
+  RF_JUMP_NEXT;
+}
+
+void rf_code_qterm(void)
+{
+  RF_START;
+  /* TODO break? */
+  RF_SP_PUSH(0);
+  RF_JUMP_NEXT;
+}
+
+void rf_code_cr(void)
+{
+  RF_START;
+  putchar('\r');
+  RF_JUMP_NEXT;
+}
+
+void rf_disc_read(char *p, uint8_t len)
+{
+  int t;
+
+  ++len;
+  while (--len) {
+    while ((t = ser_get(p)) != SER_ERR_OK) {
+    }
+    p++;
+  }
+}
+
+void rf_disc_write(char *p, uint8_t len)
+{
+  ++len;
+  while (--len) {
+    while (ser_put(*p) == SER_ERR_OVERFLOW) {
+    }
+    p++;
+  }
+}
+
+void rf_fin(void)
+{
+  ser_close();
+  ser_uninstall();
+}
