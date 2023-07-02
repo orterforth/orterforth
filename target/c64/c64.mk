f758b2023205ca8c26e14e77ee3d80aa605ab228
orterforth <orterforth@users.noreply.github.com>
2023-07-02 11:55:59 +0100

    build rules

--- target/c64/c64.mk
+++ target/c64/c64.mk
@@ -44,6 +44,41 @@ c64-run : c64/orterforth.prg
 	# 	-userportdevice 2 -rsuserdev 2 -rsuserbaud 2400 -rsdev3baud 2400 \
 	# 	-autostartprgmode 1 -autostart $<
 
+# general assemble rule
+c64/%.o : c64/%.s
+
+	ca65 -t c64 -o $@ $<
+
+# serial driver
+# TODO diagnose issue with driver built from source
+#c64/c64-up2400.s : tools/github.com/nanoflite/c64-up2400-cc65/driver/c64-up2400.ser
+c64/c64-up2400.s : target/c64/c64-up2400.ser
+
+	co65 --code-label _c64_serial -o $@ $<
+  
+# Hello World
+c64/hw.prg : hw.c | c64
+
+	cl65 -O -t c64 -o $@ $^
+
+# inst binary
+c64/inst.prg : c64/main.o c64/rf.o c64/inst.o c64/system.o c64/c64-up2400.o | c64
+
+	cl65 -O -t c64 -o $@ -m c64/inst.map $^
+
+c64/inst.s : inst.c inst.h rf.h target/c64/c64.inc | c64
+
+	cc65 -O -t c64 -DRF_ORIGIN=$(C64ORIGIN) -o $@ $<
+
+c64/main.s : main.c inst.h rf.h target/c64/c64.inc | c64
+
+	cc65 -O -t c64 -DRF_ORIGIN=$(C64ORIGIN) -o $@ $<
+
+c64/orterforth : c64/orterforth.hex | $(ORTER)
+
+	$(ORTER) hex read < $< > $@.io
+	mv $@.io $@
+
 c64/orterforth.hex : c64/inst.prg model.img | $(DISC)
 
 	@$(CHECKMEMORY) 0x801 $(C64ORIGIN) $(shell $(STAT) c64/inst.prg)
@@ -70,53 +105,19 @@ c64/orterforth.hex : c64/inst.prg model.img | $(DISC)
 
 #	mv $@.io $@
 
-# TODO specific rules listing all dependencies
-# general assemble rule
-c64/%.o : c64/%.s
-
-	ca65 -t c64 -o $@ $<
-
-# TODO rename to c64.inc
-# TODO pass ORIGIN
-# TODO hard code instead
-#
-# general compile rule
-c64/%.s : %.c | c64
-
-	cc65 -O -t c64 -DRF_ORIGIN=$(C64ORIGIN) -o $@ $<
-
-# serial driver
-# TODO diagnose issue with driver built from source
-#c64/c64-up2400.s : tools/github.com/nanoflite/c64-up2400-cc65/driver/c64-up2400.ser
-c64/c64-up2400.s : target/c64/c64-up2400.ser
-
-	co65 --code-label _c64_serial -o $@ $<
-  
-# C system lib
-c64/rf_system_c.s : target/c64/system.c | c64
-
-	cc65 -O -t c64 -o $@ $<
-
-# Hello World
-c64/hw.prg : hw.c | c64
-
-	cl65 -O -t c64 -o $@ $^
-
-# inst binary
-c64/inst.prg : c64/main.o c64/rf.o c64/inst.o c64/rf_system_c.o c64/c64-up2400.o | c64
-
-	cl65 -O -t c64 -o $@ -m c64/inst.map $^
-
-c64/orterforth : c64/orterforth.hex | $(ORTER)
-
-	$(ORTER) hex read < $< > $@.io
-	mv $@.io $@
-
 c64/orterforth.prg : c64/orterforth
 
 	printf '\001\010' > $@.io
 	cat $< >> $@.io
 	mv $@.io $@
+
+c64/rf.s : rf.c rf.h target/c64/c64.inc | c64
+
+	cc65 -O -t c64 -DRF_ORIGIN=$(C64ORIGIN) -o $@ $<
+
+c64/system.s : target/c64/system.c rf.h target/c64/c64.inc | c64
+
+	cc65 -O -t c64 -o $@ $<
 
 # Johan's serial driver
 tools/github.com/nanoflite/c64-up2400-cc65/driver/c64-up2400.s :
