58016b586875db8f5eca941e79a94bcd84029aa3
orterforth <orterforth@users.noreply.github.com>
2023-07-23 00:53:45 +0100

    minor formatting

--- z80_memory.asm
+++ z80_memory.asm
@@ -1,3 +1,5 @@
-SECTION INST                ; inst code gets own section
+SECTION INST                    ; inst code gets own section
 
-ORG (RF_ORG+RF_INST_OFFSET) ; located where it can be safely abandoned and overwritten once inst is finished
+ORG (RF_ORG+RF_INST_OFFSET)     ; located where it can be
+                                ; safely abandoned and 
+                                ; overwritten after inst
