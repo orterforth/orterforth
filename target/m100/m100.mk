d12a302b9baebb25b65c6ea0c34b564ee143b48d
orterforth <orterforth@users.noreply.github.com>
2023-07-30 16:15:47 +0100

    correct dependencies

--- target/m100/m100.mk
+++ target/m100/m100.mk
@@ -6,8 +6,13 @@ m100 :
 
 	mkdir $@
 
+.PHONY : m100-clean
+m100-clean :
+
+	rm -f m100/*
+
 .PHONY : m100-hw
-m100-hw : m100/hw.ba
+m100-hw : m100/hw.ba | $(ORTER)
 
 	@printf '  \033[1;35mOn the target type RUN "COM:38N1D" <enter>\033[0;0m\n'
 	@printf '  \033[1;35mThen on here press <enter>\033[0;0m\n'
@@ -20,23 +25,25 @@ m100/hw.ba : | m100
 	printf '20 GOTO 10\r\n\032' >> $@.io
 	mv $@.io $@
 
-m100/hw.co : | m100
+m100/hw.co : hw.c | m100
 
-	zcc +m100 -subtype=default hw.c -o $@ -create-app
+	zcc $(M100ZCCOPTS) \
+		-create-app -m -o $@ $<
 
-m100/inst.co : m100/rf.lib m100/system.lib m100/inst.lib main.c
+m100/inst.co : main.c m100/rf.lib m100/system.lib m100/inst.lib
 
-	zcc $(M100ZCCOPTS) -lm100/rf -lm100/system -lm100/inst \
-		-create-app -m -o $@ main.c
+	zcc $(M100ZCCOPTS) \
+		-lm100/rf -lm100/system -lm100/inst \
+		-create-app -m -o $@ $<
 
-m100/inst.lib : inst.c rf.h | m100
-
-	zcc $(M100ZCCOPTS) -x -o $@ $<
-
-m100/rf.lib : rf.c rf.h | m100
+m100/inst.lib : inst.c inst.h rf.h target/m100/m100.inc | m100
 
 	zcc $(M100ZCCOPTS) -x -o $@ $<
 
-m100/system.lib : target/m100/system.c rf.h | m100
+m100/rf.lib : rf.c rf.h target/m100/m100.inc | m100
+
+	zcc $(M100ZCCOPTS) -x -o $@ $<
+
+m100/system.lib : target/m100/system.c rf.h target/m100/m100.inc | m100
 
 	zcc $(M100ZCCOPTS) -x -o $@ $<
