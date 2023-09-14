6d61dd19a120b654d6b1cd6e06b6e8a75ee9ccf1
orterforth <orterforth@users.noreply.github.com>
2023-09-14 17:03:42 +0100

    m100 hw first cut

--- target/m100/m100.mk
+++ target/m100/m100.mk
@@ -6,29 +6,23 @@ m100 :
 
 	mkdir $@
 
-.PHONY : m100-clean
-m100-clean :
-
-	rm -f m100/*
-
 .PHONY : m100-hw
-m100-hw : m100/hw.ba | $(ORTER)
+m100-hw : target/m100/loader.ba m100/hw.hex | $(ORTER)
 
-	@printf '  \033[1;35mOn the target type RUN "COM:38N1D" <enter>\033[0;0m\n'
-	@printf '  \033[1;35mThen on here press <enter>\033[0;0m\n'
-	@read LINE
-	@$(ORTER) serial -e 5 $(SERIALPORT) 300 < $<
-
-m100/hw.ba : | m100
-
-	printf '10 PRINT "Hello World ";\r\n' > $@.io
-	printf '20 GOTO 10\r\n\032' >> $@.io
-	mv $@.io $@
+	@$(PROMPT) 'On the target type RUN "COM:78N1E" <enter>'
+	@printf '* \033[1;33mLoading loader\033[0;0m\n'
+	(while read -r l; do echo "$$l"; sleep 1; done < target/m100/loader.ba && printf '\032' && sleep 1) | $(ORTER) serial -o ixon -o ixoff -o onlcrx -e 2 $(SERIALPORT) 4800
+	@printf '* \033[1;33mLoading hex\033[0;0m\n'
+	(while read -r l; do echo "$$l"; sleep 2; done < m100/hw.hex && printf '\032' && sleep 1) | $(ORTER) serial -o ixon -o ixoff -o onlcrx -e 2 $(SERIALPORT) 4800
 
 m100/hw.co : hw.c | m100
 
 	zcc $(M100ZCCOPTS) \
 		-create-app -m -o $@ $<
+
+m100/hw.hex : m100/hw.co
+
+	xxd -p -u < $< > $@ 
 
 m100/inst.co : main.c m100/rf.lib m100/system.lib m100/inst.lib
 
