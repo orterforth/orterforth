57affed1edc04e2df3483bfdbb2b2e4220ba26b3
orterforth <orterforth@users.noreply.github.com>
2024-03-17 11:33:37 +0000

    Z88

--- target/z88/system.c
+++ target/z88/system.c
@@ -1,10 +1,15 @@
-/* SYSTEM BINDINGS */
+#include <rs232.h>
 
 #include "rf.h"
 
 void rf_init(void)
 {
-    fputc_cons('1');
+  if (rs232_params(RS_BAUD_9600 | RS_BITS_8 | RS_STOP_1, RS_PAR_NONE) != RS_ERR_OK) {
+    exit(1);
+  }
+  if (rs232_init() != RS_ERR_OK) {
+    exit(1);
+  }
 }
 
 void rf_code_emit(void)
@@ -23,6 +28,8 @@ void rf_code_key(void)
   RF_START;
   {
     int c = fgetc_cons();
+    /* LF -> CR  */
+    if (c == 10) c = 13;
     RF_SP_PUSH(c & 0x7F);
   }
   RF_JUMP_NEXT;
@@ -42,18 +49,65 @@ void rf_code_cr(void)
   RF_JUMP_NEXT;
 }
 
-void rf_disc_read(char *c, unsigned char len)
+void rf_disc_read(char *c, uint8_t len)
 {
-  for (; len; len--) {
+  ++len;
+  while (--len) {
+    while (rs232_get(c) == RS_ERR_NO_DATA) {
+    }
+    c++;
   }
 }
 
-void rf_disc_write(char *p, unsigned char len)
+void rf_disc_write(char *p, uint8_t len)
 {
-  for (; len; len--) {
+  ++len;
+  while (--len) {
+    while (rs232_put(*p) == RS_ERR_OVERFLOW) {
+    }
+    p++;
   }
+}
+
+void rf_code_dchar(void)
+{
+  RF_START;
+  {
+    char a, c;
+
+    a = (char) (*(rf_sp++));
+    rf_disc_read(&c, 1);
+    *(--rf_sp) = (c == a);
+    *(--rf_sp) = (c);
+  }
+  RF_JUMP_NEXT;
+}
+
+void rf_code_bread(void)
+{
+  RF_START;
+  rf_disc_read((char *) (*(rf_sp++)), RF_BBLK);
+  RF_JUMP_NEXT;
+}
+
+static char eot = 0x04;
+
+void rf_code_bwrit(void)
+{
+  RF_START;
+  {
+    uint8_t a = (uint8_t) (*(rf_sp++));
+    char *b = (char *) (*(rf_sp++));
+
+    rf_disc_write(b, a);
+    rf_disc_write(&eot, 1);
+  }
+  RF_JUMP_NEXT;
 }
 
 void rf_fin(void)
 {
+  if (rs232_close() != RS_ERR_OK) {
+    exit(1);
+  }
 }
--- target/z88/z88.mk
+++ target/z88/z88.mk
@@ -1,12 +1,38 @@
-Z88MAMEOPTS := z88 $(MAMEOPTS)
+# === Cambridge Z88 ===
 
-Z88ORG := 0x0000
-Z88ORIGIN := 0x0000
+# Z88OPTION := default
+Z88OPTION := assembly
+ifeq ($(TARGET),z88)
+ifneq ($(OPTION),)
+Z88OPTION := $(OPTION)
+endif
+endif
+
+Z88DEPS := z88/inst.lib z88/rf.lib z88/system.lib main.c
+Z88IMPEXPORT := \
+	$(PROMPT) "On the Z88 go to: Imp-Export and R)eceive file" && \
+	$(INFO) "Importing file" && \
+	$(ORTER) serial -e 15 $(SERIALPORT) $(SERIALBAUD) <
+Z88LIBS := -lm -lz88/inst -lz88/rf -lz88/system
+# PAGE
+Z88ORG := 0x2300
+Z88ORIGIN := 0x3E00
+
+ifeq ($(M100OPTION),assembly)
+	Z88DEPS += z88/rf_z80.lib
+	Z88LIBS += -lz88/rf_z80
+	Z88ORIGIN := 0x3300
+endif
 
 Z88ZCCOPTS := +z88 \
 	-DRF_ORG=$(Z88ORG) \
 	-DRF_ORIGIN=$(Z88ORIGIN) \
+	-Ca-DRF_ORIGIN=$(Z88ORIGIN) \
 	-DRF_TARGET_INC='\"target/z88/system.inc\"'
+
+ifeq ($(M100OPTION),assembly)
+	Z88ZCCOPTS += -DRF_ASSEMBLY
+endif
 
 z88 :
 
@@ -15,42 +41,52 @@ z88 :
 .PHONY : z88-hw
 z88-hw : z88/hw.imp
 
-	$(ORTER) serial -o ixon -o ixoff -e 15 $(SERIALPORT) 9600 < $<
+	@$(Z88IMPEXPORT) $<
+	@$(WARN) 'Now go to BBC Basic and RUN "hw"'
 
-.PHONY : z88-inst
-z88-inst : z88/inst.imp
+.PHONY : z88-install
+z88-install : z88/orterforth.imp
 
-	$(ORTER) serial -o ixon -o ixoff -e 15 $(SERIALPORT) 9600 < $<
+	@$(Z88IMPEXPORT) $<
+	@$(WARN) 'Now go to BBC Basic and RUN "orterforth"'
 
-.PHONY : z88-run
-z88-run :
+z88/%.imp : z88/%.bin
 
-	mame $(Z88MAMEOPTS)
+	$(ORTER) z88 imp-export write $(*F) < $< > $@.io
+	mv $@.io $@
 
 z88/hw.bin : hw.c | z88
 
 	zcc $(Z88ZCCOPTS) -o $@ $<
 
-z88/hw.imp : z88/hw.bin
+z88/inst.bin z88/inst.map : $(Z88DEPS)
 
-	$(ORTER) z88 imp-export write HW < $< > $@.io
-	mv $@.io $@
-
-z88/inst.bin z88/inst.map : z88/rf.lib z88/system.lib z88/inst.lib main.c
-
-	zcc $(Z88ZCCOPTS) -lm -lz88/rf -lz88/system -lz88/inst \
-		-m -o z88/inst.bin main.c
-
-z88/inst.imp : z88/inst.bin
-
-	$(ORTER) z88 imp-export write INST < $< > $@.io
-	mv $@.io $@
+	zcc $(Z88ZCCOPTS) $(Z88LIBS) -m -o z88/inst.bin main.c
 
 z88/inst.lib : inst.c rf.h | z88
 
 	zcc $(Z88ZCCOPTS) -x -o $@ $<
 
+z88/orterforth.bin : z88/orterforth.hex | $(ORTER)
+
+	$(ORTER) hex read < $< > $@
+
+z88/orterforth.hex : z88/inst.imp z88/inst.bin model.img
+
+	@$(CHECKMEMORY) $(Z88ORG) $(Z88ORIGIN) $$($(STAT) z88/inst.bin)
+	@$(Z88IMPEXPORT) $<
+	@$(WARN) 'Now go to BBC Basic and RUN "inst"'
+	@$(EMPTYDR1FILE) $@.io
+	@$(STARTDISC) serial $(SERIALPORT) $(SERIALBAUD) model.img $@.io
+	@$(WAITUNTILSAVED) $@.io
+	@$(STOPDISC)
+	@$(COMPLETEDR1FILE)
+
 z88/rf.lib : rf.c rf.h target/z88/system.inc | z88
+
+	zcc $(Z88ZCCOPTS) -x -o $@ $<
+
+z88/rf_z80.lib : rf_z80.asm | z88
 
 	zcc $(Z88ZCCOPTS) -x -o $@ $<
 
