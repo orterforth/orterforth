cda80d37316f6487513f9313732574c1a0274fac
orterforth <orterforth@users.noreply.github.com>
2024-03-17 12:15:42 +0000

    colour memory check

--- scripts/check-memory.sh
+++ scripts/check-memory.sh
@@ -10,9 +10,10 @@ ORG=$(($1))
 ORIGIN=$(($2))
 LENGTH=$3
 
-echo "ORG=$ORG ORIGIN=$ORIGIN LENGTH=$LENGTH"
-
 if [ $(( $ORG + $LENGTH )) -gt $(( $ORIGIN )) ]; then
-    echo "code overlaps with ORIGIN"
+    printf "  \033[1;31mORG=%04X ORIGIN=%04X LENGTH=%04X\033[0;0m\n" $ORG $ORIGIN $LENGTH
+    printf "  \033[1;31mcode overlaps with ORIGIN\033[0;0m\n"
     exit 1
+else
+    printf "  \033[1;32mORG=%04X ORIGIN=%04X LENGTH=%04X\033[0;0m\n" $ORG $ORIGIN $LENGTH
 fi
