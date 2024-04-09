d08fe67b2aa38bec766fd83c0a56d57528f25ba1
orterforth <orterforth@users.noreply.github.com>
2024-04-09 21:17:38 +0100

    use io.c

--- target/apple2/apple2.mk
+++ target/apple2/apple2.mk
@@ -17,11 +17,15 @@ apple2/hw : hw.c | apple2
 
 	cl65 -O -t apple2 -o $@ $^
 
-apple2/inst apple2/inst.map : apple2/inst.o apple2/main.o apple2/rf.o apple2/system.o
+apple2/inst apple2/inst.map : apple2/inst.o apple2/io.o apple2/main.o apple2/rf.o apple2/system.o
 
 	cl65 -O -t apple2 -o $@ -m apple2/inst.map $^
 
 apple2/inst.s : inst.c inst.h rf.h target/apple2/apple2.inc | apple2
+
+	cc65 $(APPLE2CC65OPTS) -o $@ $<
+
+apple2/io.s : io.c rf.h target/apple2/apple2.inc | apple2
 
 	cc65 $(APPLE2CC65OPTS) -o $@ $<
 
--- target/apple2/system.c
+++ target/apple2/system.c
@@ -1,52 +1,33 @@
 #include <stdint.h>
 
-#include "../../rf.h"
-
 void rf_init(void)
 {
 }
 
-void rf_code_emit(void)
-{
-  RF_START;
-  {
-    uint8_t c;
-    
-    c = RF_SP_POP & 0x7F;
-    RF_USER_OUT++;
-  }
-  RF_JUMP_NEXT;
-}
-
-void rf_code_key(void)
-{
-  RF_START;
-  {
-    int c;
-
-    RF_SP_PUSH(c & 0x7F);
-  }
-  RF_JUMP_NEXT;
-}
-
-void rf_code_qterm(void)
-{
-  RF_START;
-  RF_SP_PUSH(0);
-  RF_JUMP_NEXT;
-}
-
-void rf_code_cr(void)
-{
-  RF_START;
-  RF_JUMP_NEXT;
-}
-
-void rf_disc_read(char *p, uint8_t len)
+void rf_console_put(uint8_t b)
 {
 }
 
-void rf_disc_write(char *p, uint8_t len)
+uint8_t rf_console_get(void)
+{
+  return 0;
+}
+
+uint8_t rf_console_qterm(void)
+{
+  return 0;
+}
+
+void rf_console_cr(void)
+{
+}
+
+uint8_t rf_serial_get(void)
+{
+  return 0;
+}
+
+void rf_serial_put(uint8_t b)
 {
 }
 
