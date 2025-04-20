330dcde51899b10360f8471afbb871cf74b20254
orterforth <orterforth@users.noreply.github.com>
2025-04-20 15:39:06 +0100

    BDOS

--- target/cpm/cpm.mk
+++ target/cpm/cpm.mk
@@ -5,19 +5,19 @@ cpm :
 	mkdir $@
 
 .PHONY : cpm-build
-cpm-build : cpm/inst.com
+cpm-build : cpm/inst.dsk
 
 cpm/%.lib : %.c rf.h target/cpm/cpm.inc | cpm
 
 	zcc $(CPMZCCOPTS) -x -o $@ $<
 
-cpm/hw.com : hw.c | cpm
+cpm/hw.bin cpm/hw.com cpm/hw.dsk : hw.c | cpm
 
-	zcc $(CPMZCCOPTS) -o $@ $<
+	zcc $(CPMZCCOPTS) -o cpm/hw.bin $< -create-app -subtype=z80pack
 
-cpm/inst.com cpm/inst.map : cpm/inst.lib cpm/io.lib cpm/rf.lib cpm/system.lib main.c
+cpm/inst.bin cpm/inst.com cpm/inst.dsk : cpm/inst.lib cpm/io.lib cpm/rf.lib cpm/system.lib main.c
 
-	zcc $(CPMZCCOPTS) -lcpm/inst -lcpm/io -lcpm/rf -lcpm/system -m -o $@ main.c
+	zcc $(CPMZCCOPTS) -lcpm/inst -lcpm/io -lcpm/rf -lcpm/system -m -o cpm/inst.bin main.c -create-app -subtype=z80pack
 
 cpm/system.lib : target/cpm/system.c rf.h target/cpm/cpm.inc | cpm
 
--- target/cpm/system.c
+++ target/cpm/system.c
@@ -1,19 +1,18 @@
+#include <cpm.h>
 #include <stdint.h>
-#include <stdio.h>
 
 void rf_init(void)
 {
-  fputc_cons('A');
 }
 
 uint8_t rf_console_get(void)
 {
-  return fgetc_cons();
+  return bdos(CPM_RCON, 0);
 }
 
 void __FASTCALL__ rf_console_put(uint8_t b)
 {
-  fputc_cons(b);
+  bdos(CPM_WCON, b);
 }
 
 uint8_t rf_console_qterm(void)
@@ -23,16 +22,18 @@ uint8_t rf_console_qterm(void)
 
 void rf_console_cr(void)
 {
-  fputc_cons('\n');
+  bdos(CPM_WCON, 13);
 }
 
 uint8_t rf_serial_get(void)
 {
-  return 0;
+  return bdos(CPM_RRDR, 0);
 }
 
 void __FASTCALL__ rf_serial_put(uint8_t b)
 {
+  bdos(CPM_WCON, b);
+  bdos(CPM_WPUN, b);
 }
 
 void rf_fin(void)
