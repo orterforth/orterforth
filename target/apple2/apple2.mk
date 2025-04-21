59363a13308b69ef03ab4ab5f24e714029b52cde
orterforth <orterforth@users.noreply.github.com>
2025-04-21 13:05:40 +0100

    minor step towards hw

--- target/apple2/apple2.mk
+++ target/apple2/apple2.mk
@@ -2,6 +2,11 @@ apple2 :
 
 	mkdir $@
 
+.PHONY : apple2-hw
+apple2-hw : apple2/hw
+
+	mame apple2 $(MAMEOPTS) -flop1 apple2/hw
+
 apple2/%.o : apple2/%.s
 
 	ca65 -t apple2 -o $@ $<
@@ -12,7 +17,7 @@ apple2/%.s : %.c rf.h target/apple2/apple2.inc | apple2
 
 apple2/hw apple2/hw.map : apple2/hw.o
 
-	cl65 -O -t apple2 -o $@ -m apple2/hw.map $^
+	cl65 -O -t apple2 -o $@ $^
 
 apple2/inst apple2/inst.map : apple2/inst.o apple2/io.o apple2/main.o apple2/rf.o apple2/system.o
 
